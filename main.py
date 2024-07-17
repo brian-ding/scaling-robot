d60bddbe12066dae3f46ca25c83683367836fa79
Castle <xinghan.zhu@partner.bmw.com>
2024-07-17 16:37:59 +0800

    change token  to env

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def cli():
 @cli.command()
 @click.option('-r', '--repo-name', required=True, help='The Repo to review')
 @click.option('-n', '--pr-num', required=True, type=int, help='The PR number')
-@click.option('-t', '--token', required=True, help='Github token')
+@click.option('--token', envvar='GH_TOKEN', help='Github token')
 def review(repo_name,pr_num,token):
     """Review a PR"""
     click.echo(f"Reviewing {repo_name}/{pr_num}/{token}")
@@ -15,7 +15,7 @@ def review(repo_name,pr_num,token):
 @cli.command()
 @click.option('-r', '--repo-name', required=True, help='The Repo to review')
 @click.option('-n', '--pr-num', required=True, type=int, help='The PR number')
-@click.option('-t', '--token', required=True, help='Github token')
+@click.option('--token', envvar='GH_TOKEN', help='Github token')
 def summary(repo_name,pr_num,token):
     """Summarize a PR"""
     click.echo(f"Summarizing {repo_name}/{pr_num}/{token}")

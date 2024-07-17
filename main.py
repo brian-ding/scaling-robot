b69985dce74f09ad6d60b2a9ab00a1e4952b2178
Hui Guan <hui.guan@bmw.com>
2024-07-17 19:07:41 +0800

    Add Git ignore file and main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+github_token = os.getenv("GITHUB_TOKEN")
+
+def review_pr(pr_number):
+    """
+    Review PR for the specified PR number
+     TODO
+    """
+    pass
+
+def summarize_pr(pr_number):
+    """
+    Summarize PR for the specified PR number
+    TODO
+    """
+    pass

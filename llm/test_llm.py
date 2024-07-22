b2d366623f55004af5b4bd32b8a1f4d442b01b59
Hui <guanhui629@gmail.com>
2024-07-22 14:10:12 +0800

    Hui/refactor/main (#12)
    * Initial commit
    
    * Update README.md (#1)
    
    * init llm layout (#2)
    
    * feat: [Noticket] add command entry point (#3)
    
    * feat: [Noticket] add command entry point
    
    we define two command as entry point
    python main.py summary -r {repo_name} -n {pr_num}
    python main.py review -r {repo_name} -n {pr_num}
    
    * add token in input
    
    * change token  to env
    
    * add github module
    
    * add init file for gh module
    
    * resolve circle calling
    
    * add ignore file
    
    * use GH_TOKEN without login
    
    * Update ghapi.py
    
    ---------
    
    Co-authored-by: Castle <xinghan.zhu@partner.bmw.com>
    
    * move to gh folder (#5)
    
    * add comment_on_pr (#6)
    
    Co-authored-by: Leon Su <Leon.su@bmw.com>
    
    * Update main review method (#7)
    
    * [gh] update review method in main
    
    * Add idea config
    
    * Comment the comment_on_pr
    
    ---------
    
    Co-authored-by: Hui Guan <hui.guan@bmw.com>
    
    * Pr summary (#8)
    
    * talk to ollama
    
    * get the host from env
    
    * generate summary prompt
    
    * summarize pr
    
    * change comment on _ask
    
    * combine pr info & add diff url enable (#9)
    
    * combine pr info & add diff url enable
    
    * add diff info
    
    ---------
    
    Co-authored-by: Leon Su <Leon.su@bmw.com>
    
    * integrate summary_pr_info (#10)
    
    Co-authored-by: Castle <xinghan.zhu@partner.bmw.com>
    
    * fix import issue (#11)
    
    * Add UT & add code review prompt
    
    * Add UT and Review prompt
    
    * Update model based on host url
    
    ---------
    
    Co-authored-by: Brian Ding <brian_ding@outlook.com>
    Co-authored-by: Hanger <30338453+zhutoutou@users.noreply.github.com>
    Co-authored-by: Castle <xinghan.zhu@partner.bmw.com>
    Co-authored-by: Br1ze <suchbreeze@163.com>
    Co-authored-by: Leon Su <Leon.su@bmw.com>
    Co-authored-by: Hui Guan <hui.guan@bmw.com>

---
+++ llm/test_llm.py
@@ -0,0 +1,60 @@
+import unittest
+import json
+from llm.llmapi import summarize_pr_info, _generate_summary_messages, review_pr_code, _ask
+from gh.pr_info import PRInfo
+from gh.pr_code import PRCode
+from llm._message import Message, Role
+import os
+import requests
+
+
+class TestLlm(unittest.TestCase):
+
+    def setUp(self):
+        # Set up necessary environment variables
+        self.llm_host = os.getenv('LLM_HOST', 'http://localhost:11434')
+        # Load PR info from JSON file
+        with open('test_pr_info.json', 'r') as file:
+            pr_info_data = json.load(file)
+            self.pr_info = PRInfo(
+                repo=pr_info_data['repo'],
+                prNumber=pr_info_data['prNumber'],
+                title=pr_info_data['title'],
+                description=pr_info_data['description'],
+                diff=pr_info_data['diff']
+            )
+
+    def test_summarize_pr_info(self):
+        # Test summarizing PR information
+        result = summarize_pr_info(self.pr_info)
+        self.assertTrue("PR summary result:" in result)
+
+    def test_generate_summary_messages(self):
+        # Test generating summary messages
+        result = _generate_summary_messages(self.pr_info)
+        expected_message = Message(Role.USER,
+                                   f"Can you summarize the PR based on the following info?\nthe title is: {self.pr_info.title}\nand the description is: {self.pr_info.description}")
+        self.assertEqual(result, [expected_message])
+
+    def test_review_pr_code(self):
+        result = review_pr_code(self.pr_info)
+        self.assertTrue(result)
+
+    def test_ask(self):
+        # Test asking the LLM a question
+        messages = [Message(Role.USER, "fake content")]
+        url = f"{self.llm_host}/api/chat"
+        payload = {
+            "model": "llama3" if 'localhost' in self.llm_host else "llama3:8b",
+            # The name is llama3 for the local installation
+            # "model": "llama3:8b",
+            "messages": [message.to_dict() for message in messages],
+            "stream": False
+        }
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(url, json=payload, headers=headers)
+        self.assertTrue(response.text)
+
+
+if __name__ == '__main__':
+    unittest.main()
